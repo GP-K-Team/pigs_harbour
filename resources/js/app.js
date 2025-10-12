c8afb7534cb61a4dcb4db3574b4e1a43a03aae38
whatevernumber <the_lost_number@mail.ru>
2025-10-12 17:26:10 +0300

    JQUERY

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,10 @@ window.$ = window.jQuery = $
 
 $(document).ready(function() {
     $('.menu_burger').on('click', function() {
-        $('.mobile_nav_wrapper').css('display', 'block');
+        $('.mobile_nav_wrapper').show();
     });
 
     $('.close_nav_button').on('click', function() {
-        $('.mobile_nav_wrapper').css('display', 'none');
+        $('.mobile_nav_wrapper').hide();
     });
 });
