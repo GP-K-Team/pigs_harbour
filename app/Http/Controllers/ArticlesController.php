60a8596892f98a316a8c5cd6710d144ded51255f
whatevernumber <the_lost_number@mail.ru>
2025-11-10 19:48:51 +0300

    Remove unused

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Helpers\PageTextHelper;
-use App\Http\Requests\ArticleFormRequest;
 use App\Http\Requests\Article\CreateArticleFormRequest;
 use App\Http\Requests\Article\UpdateArticleFormRequest;
 use App\Models\Article;
