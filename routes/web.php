a038b450d724d33d45ba02ab02a3fc88fc522450
the-nepodarok <kosov.a@list.ru>
2025-11-14 00:55:34 +0300

    added fallback route

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,7 @@ Route::middleware('auth:web')->group(function () {
    Route::delete('files/{file}', [FileController::class, 'delete']);
    Route::put('page_text/{pageText}', [PageTextController::class, 'update']);
 });
+
+Route::fallback(function () {
+    return \view('errors.404');
+});
