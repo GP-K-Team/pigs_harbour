b712b51ed49d4dfdb5dbc199e59e608ffd77347c
the-nepodarok <kosov.a@list.ru>
2026-04-18 21:27:58 +0300

    removed unused job

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use App\Jobs;
 use App\Console\Commands;
 use Illuminate\Support\Facades\Schedule;
 
-// Schedule::job(Jobs\GenerateSitemap::class)->dailyAt(20);
 Schedule::command(Commands\CleanupStorage::class)->saturdays()->at('10:00');
