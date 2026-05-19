beffa3e53df8b1be4e0f0f50ac1ddffd87367a3b
whatevernumber <the_lost_number@mail.ru>
2026-05-19 21:57:49 +0300

    add favicon variation

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -49,13 +49,6 @@
 
         <script src="https://vk.com/js/api/openapi.js?169" type="text/javascript"></script>
 
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
-        <link rel="shortcut icon" href="/favicon.ico">
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <meta name="apple-mobile-web-app-title" content="Пристань Пушистых Сердец">
         <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
