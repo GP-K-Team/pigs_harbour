a56ef25664b3fe2920c7f9f09d4085aa70f5c9ff
whatevernumber <the_lost_number@mail.ru>
2026-05-19 21:56:04 +0300

    add favicon variation

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,6 +45,7 @@
 
         <script src="https://vk.com/js/api/openapi.js?169" type="text/javascript"></script>
 
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
