b6c607d72925c409eaab5537aeae1dd6ced552ab
whatevernumber <the_lost_number@mail.ru>
2026-06-27 21:45:25 +0300

    food products wip

---
+++ database/migrations/2026_06_23_114625_create_food_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('food_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->index();
+            $table->string('slug_title')->index();
+            $table->string('synonyms');
+            $table->string('description');
+            $table->string('type');
+            $table->text('text');
+            $table->string('meta_title')->nullable();
+            $table->string('meta_description')->nullable();
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('food_products');
+    }
+};
