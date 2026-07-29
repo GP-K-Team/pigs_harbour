7e17cf98d9de40304a9f1e922ba38b44c96eff08
whatevernumber <the_lost_number@mail.ru>
2026-07-29 21:46:22 +0300

    Fix: transform trait into an attribute

--- app/Attributes/RichContentColumnName.php
+++ app/Attributes/RichContentColumnName.php
@@ -6,6 +6,9 @@ use App\Attributes\RetrievableAttribute;
 
 use Attribute;
 
+/**
+ * @method string getRichContentColumnName
+ */
 #[Attribute(Attribute::TARGET_CLASS)]
 readonly class RichContentColumnName extends RetrievableAttribute
 {
